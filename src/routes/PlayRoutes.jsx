03584d5243b20dc55d5f64fd36664d183d9b30c9
JJuHS <ghtjd006908@gmail.com>
2025-04-02 11:29:54 +0900

    paint to server

--- src/routes/PlayRoutes.jsx
+++ src/routes/PlayRoutes.jsx
@@ -1,26 +1,65 @@
-import React from "react";
+// import React from "react";
+// import { Route, Routes } from 'react-router-dom';
+// import Play from '../components/Play/Play.jsx';
+
+// function Paint() {
+//     return (
+//         <div className="w-screen h-screen overflow-hidden flex flex-row">
+//             <div className="w-1/5"></div>
+//             <iframe 
+//             src={`${process.env.PUBLIC_URL}/for_html/paint/paint.html`} 
+//             className="w-3/5 rounded block"
+//             />
+//         </div>
+//     )
+// }
+
+// function PlayRoutes () {
+//     return (
+//         <Routes>
+//             <Route path="/" element={<Play />} />
+//             <Route path="/paint" element={<Paint/>}></Route>
+//         </Routes>
+//     )
+// }
+
+// export default PlayRoutes;
+
+
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Play from '../components/Play/Play.jsx';
 
 function Paint() {
+    const [htmlContent, setHtmlContent] = useState('');
+
+    useEffect(() => {
+        fetch(`${process.env.PUBLIC_URL}/for_html/paint/paint.txt`)
+            .then(response => response.text())
+            .then(data => {
+                setHtmlContent(data);
+            })
+            .catch(error => console.error('Error loading the HTML:', error));
+    }, []);
+
     return (
         <div className="w-screen h-screen overflow-hidden flex flex-row">
             <div className="w-1/5"></div>
-            <iframe 
-            src={`${process.env.PUBLIC_URL}/for_html/paint/paint.html`} 
-            className="w-3/5 rounded block"
+            <div
+                className="w-3/5 rounded block overflow-auto"
+                dangerouslySetInnerHTML={{ __html: htmlContent }}
             />
         </div>
-    )
+    );
 }
 
-function PlayRoutes () {
+function PlayRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Play />} />
-            <Route path="/paint" element={<Paint/>}></Route>
+            <Route path="/paint" element={<Paint />} />
         </Routes>
-    )
+    );
 }
 
 export default PlayRoutes;
